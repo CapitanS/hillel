d6ea3b8eaf596f2ed7bc220edcd2f418a97632d5
dimat <dimaprizma2016@gmail.com>
2020-11-27 16:10:24 +0200

    Wrote function parse.

--- Homeworks/hw02_git/hw02_main.py
+++ Homeworks/hw02_git/hw02_main.py
@@ -2,9 +2,21 @@
 # hw02_main.py - practicing git skills
 
 
-#
+# Function is parsing url and return dictionary with parameters for web-server.
+# Parameters are the list of couples key/value, which separated by symbol &.
 def parse(query: str) -> dict:
-    return {}
+    dict_of_parameters = {}
+    string_of_parameters = query.rpartition('?')
+    if string_of_parameters[0] and string_of_parameters[2]:
+        list_of_parameters = string_of_parameters[2].split('&')
+        for i in range(len(list_of_parameters)):
+            if list_of_parameters[i]:
+                key, value = list_of_parameters[i].split('=')
+                dict_of_parameters.setdefault(key, value)
+    else:
+        pass
+        # list_of_parameters = None
+    return dict_of_parameters
 
 
 if __name__ == '__main__':
@@ -13,3 +25,4 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    # TODO Write 10 assert for testing
