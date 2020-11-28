8f8953f4a71e7d41fb7ce59776f17ae9dc4afa52
dimat <dimaprizma2016@gmail.com>
2020-11-28 13:17:26 +0200

    Refactored function parse. + parse checks for an anchor in url + moved into a separate function to fill up the dictionary from a list by separator Added 10 asserts for testing

--- Homeworks/hw02_git/hw02_main.py
+++ Homeworks/hw02_git/hw02_main.py
@@ -2,20 +2,32 @@
 # hw02_main.py - practicing git skills
 
 
-# Function is parsing url and return dictionary with parameters for web-server.
+# Function fills a dictionary from the list by separator
+def from_list_to_dict_by_separator(list: list, dict: dict) -> dict:
+    separator = '='
+    for i in range(len(list)):
+        if list[i]:
+            key, value = list[i].split(separator)
+            dict.setdefault(key, value)
+    return dict
+
+
+# Function parses url and return dictionary with parameters for web-server.
 # Parameters are the list of couples key/value, which separated by symbol &.
 def parse(query: str) -> dict:
     dict_of_parameters = {}
-    string_of_parameters = query.rpartition('?')
-    if string_of_parameters[0] and string_of_parameters[2]:
-        list_of_parameters = string_of_parameters[2].split('&')
-        for i in range(len(list_of_parameters)):
-            if list_of_parameters[i]:
-                key, value = list_of_parameters[i].split('=')
-                dict_of_parameters.setdefault(key, value)
+    string_of_parameters = query.partition('?')
+    if string_of_parameters[1] and string_of_parameters[2]:
+        # check for an anchor in url
+        if string_of_parameters[2].find('#') == -1:
+            list_of_parameters = string_of_parameters[2].split('&')
+            from_list_to_dict_by_separator(list_of_parameters, dict_of_parameters)
+        else:
+            # detach the anchor and split the parameters
+            list_of_parameters = string_of_parameters[2].split('#')[0].split('&')
+            from_list_to_dict_by_separator(list_of_parameters, dict_of_parameters)
     else:
-        pass
-        # list_of_parameters = None
+        return dict_of_parameters
     return dict_of_parameters
 
 
@@ -25,4 +37,15 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-    # TODO Write 10 assert for testing
+    # New 10 asserts for testing
+    assert parse('http://example.com/?name=Dima#') == {'name': 'Dima'}
+    assert parse('http://?') == {}
+    assert parse('http://example.com/?name=Dima#anchor') == {'name': 'Dima'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&age=28') ==\
+           {'name': 'ferret', 'color': 'purple', 'age': '28'}
+    assert parse('http://') == {}
+    assert parse('http') == {}
+    assert parse('http://example.com/?name=') == {'name': ''}
+    assert parse('http://example') == {}
+    assert parse('http://example?') == {}
+    assert parse('https://example.com/path/to/page/') == {}
