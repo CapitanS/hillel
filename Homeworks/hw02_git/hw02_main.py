b97657dbc4628815c31953bfd724da202c52971c
dimat <dimaprizma2016@gmail.com>
2020-11-28 13:38:17 +0200

    Created the branch "feature_def_parse_cookie" and filled the description "parse_cookie".

--- Homeworks/hw02_git/hw02_main.py
+++ Homeworks/hw02_git/hw02_main.py
@@ -1,3 +1,15 @@
 #! python3
 # hw02_main.py - practicing git skills
 
+
+# Function parses string of cookies separated by ';' and return dictionary with cookies
+# where the key is an information before '=' and value after
+def parse_cookie(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
